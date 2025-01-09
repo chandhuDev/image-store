2bcb533ed205e70cfa7bb42b65e42918fcd7467e
chandhu <chandhudev0@gmail.com>
2025-01-09 15:23:44 +0530

    update: added the layout and assets to the repo

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4a71697d38127eef"; }
+/******/ 		__webpack_require__.h = function() { return "5b9397ef69ae60b6"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,57 @@
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchAllPosts } from "../redux/slices/postSlice"; // Updated import
-import ImageGrid from "../components/Home/ImageGrid";
-import UploadModal from "../components/Home/UploadModal";
-import Spinner from "../components/Home/Spinner";
+// pages/index.js
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchAllPosts } from '../redux/slices/postSlice';
+import Layout from '../components/Home/Layout';
+import ImageGrid from '../components/Home/ImageGrid';
+import Spinner from '../components/Home/Spinner';
 
 export default function Home() {
   const dispatch = useDispatch();
-  const { posts, loading } = useSelector((state) => state.posts);
+  const { posts, loading, error } = useSelector((state) => state.posts);
 
   useEffect(() => {
     dispatch(fetchAllPosts());
   }, [dispatch]);
 
-  if (loading) return <Spinner message="Rendering the page"/>;
-
   return (
-    <div className="min-h-screen bg-gray-100">
-      <ImageGrid posts={posts} />
-    </div>
+    <Layout>
+      <div className="flex flex-col">
+        <div className="flex flex-col justify-center items-center">
+          <h1 className="text-3xl font-bold text-center mb-8">
+            Share Your Amazing Images
+          </h1>
+          
+          {loading && <Spinner />}
+          
+          {error && (
+            <div className="text-red-500 text-center mb-8">
+              {error}
+            </div>
+          )}
+          
+          {!loading && !error && posts?.length === 0 && (
+            <div className="flex flex-col items-center justify-center min-h-[400px]">
+              <div className="text-gray-500 text-xl text-center mb-4">
+                No posts yet
+              </div>
+              <p className="text-gray-400">
+                Be the first to share your amazing images!
+              </p>
+              <button 
+                onClick={() => {/* Open upload modal */}}
+                className="mt-4 bg-blue-500 text-white px-6 py-2 rounded-lg hover:bg-blue-600"
+              >
+                Create Post
+              </button>
+            </div>
+          )}
+          
+          {!loading && !error && posts?.length > 0 && (
+            <ImageGrid posts={posts} />
+          )}
+        </div>
+      </div>
+    </Layout>
   );
 }
